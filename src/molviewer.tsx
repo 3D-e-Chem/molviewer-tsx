70191f568cb187706d460145785d61e4610194f5
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2016-10-07 12:22:33 +0200

    Use tsx

--- src/molviewer.tsx
+++ src/molviewer.tsx
@@ -4,4 +4,4 @@ import {HelloWorld} from './hello';
 import 'bootstrap/dist/css/bootstrap.css!';
 
 let container = document.getElementById('container');
-let component = ReactDOM.render(React.createElement(HelloWorld), container);
+let component = ReactDOM.render(<HelloWorld/>, container);
