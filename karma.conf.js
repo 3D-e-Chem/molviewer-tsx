6edecc5131cfb773df39b4b8c3ccfe2bd876a678
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2016-10-09 14:53:19 +0200

    Coverage offered by https://github.com/Workiva/karma-jspm/pull/178

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,8 @@ module.exports = function (config) {
     jspm: {
       config: "jspm.config.js",
       loadFiles: ['src/**/*.spec.tsx'],
-      serveFiles: ['src/**/*.tsx']
+      serveFiles: ['src/**/*.tsx'],
+      coverage: ['src/**/!(*.spec).tsx']
     },
     proxies: {
       '/src/': '/base/src/',
@@ -17,15 +18,30 @@ module.exports = function (config) {
     },
     reporters: [
       'progress',
-      'mocha'
+      'mocha',
+      'jspm',
     ],
     mochaReporter: {
       output: 'minimal',
       showDiff: 'unified'
     },
+    coverageReporter: {
+      dir: 'test_results/coverage/',
+      reporters: [
+        {
+          type: 'cobertura',
+          subdir: './',
+          file: 'cobertura.xml'
+        }, {
+          type: 'html',
+          subdir: './report_html'
+        }
+      ],
+      includeAllSources: true
+    },
     port: 9876,
     colors: true,
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['Chrome'],
     singleRun: true,
