1109b0c751f7a67988a180b3d879f4ee88808e2d
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2016-11-07 13:48:49 +0100

    Don't use lambdas inside jsx

--- src/molcanvas.tsx
+++ src/molcanvas.tsx
@@ -13,6 +13,11 @@ export class MolCanvas extends React.Component<IMolCanvasProps, {}> {
     private canvasContainerEl: Element;
     private viewer: $3Dmol.IGLViewer;
 
+    constructor() {
+        super();
+        this.canvasRefHandler = this.canvasRefHandler.bind(this);
+    }
+
     public render() {
         const ligands = this.props.ligands.map((ligand) => (
             <LigandGLModel key={ligand.id} {...ligand} viewer={this.viewer} />
@@ -20,7 +25,7 @@ export class MolCanvas extends React.Component<IMolCanvasProps, {}> {
         const proteins = this.props.proteins.map((protein) => (
             <ProteinGLModel key={protein.id} {...protein} viewer={this.viewer} />
         ));
-        return <div style={{ height: '100%', width: '100%' }} ref={(c) => this.canvasContainerEl = c}>
+        return <div style={{ height: '100%', width: '100%' }} ref={this.canvasRefHandler}>
             {ligands}
             {proteins}
         </div>;
@@ -32,5 +37,9 @@ export class MolCanvas extends React.Component<IMolCanvasProps, {}> {
         this.viewer = $3Dmol.createViewer(element, config);
         this.viewer.setBackgroundColor(0xffffff);
         this.viewer.render();
+    }
+
+    private canvasRefHandler(ref: Element) {
+        this.canvasContainerEl = ref;
     }
 }
