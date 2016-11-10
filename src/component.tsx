b335e86de9cb14a37502268b4444d7e01b8d2749
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2016-11-10 11:30:13 +0100

    Renamed *.tsx files which have no jsx to *.ts.
    Adjusted tslint to prefer as cast.
    
    Also removed unused files.

--- src/component.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {MolContainer} from './molcontainer';
-
-export class HelloWorld extends React.Component<{}, {}> {
-  render() {
-    return <div><h1>Mol viewer</h1><MolContainer></MolContainer></div>;
-  }
-}
--- src/molviewer.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {HelloWorld} from './component';
-
-let container = document.getElementById('container');
-let component = ReactDOM.render(React.createElement(HelloWorld), container);
