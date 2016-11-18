66c10fbd1b2d5fe8bc7220dea01b877227cdd5b1
AJK-dev <kooistra.a.j@gmail.com>
2016-11-18 08:14:46 +0100

    Don't forget Edge ;)

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 rsync -a --exclude \*.map --exclude asset-manifest.json build/ $1
 
-# remove source map urls, to make Safari happy
+# remove source map urls, to make Safari and Edge happy
 perl -pi -e 's!^//# sourceMappingURL=.*$!!' $1/static/js/main.*.js $1/static/css/main.*.css
