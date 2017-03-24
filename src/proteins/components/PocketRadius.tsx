774cd98e14e89b6f445e87c7089ce4a525d0dedf
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-03-24 08:12:24 +0100

    Use camelCase style keys

--- src/proteins/components/PocketRadius.tsx
+++ src/proteins/components/PocketRadius.tsx
@@ -16,7 +16,7 @@ type IProps = IStateProps & IDispatchProps;
 export const PocketRadius = (props: IProps) => {
     return <div>
         <p>Pocket selection radius (Å): <span>{props.value}</span></p>
-        <div style={{'padding-left': 7, 'padding-right': 7}}>
+        <div style={{paddingLeft: 7, paddingRight: 7}}>
             <Slider min={2.5} max={10} step={0.5} value={props.value} onChange={props.onChange}/>
         </div>
     </div>;
