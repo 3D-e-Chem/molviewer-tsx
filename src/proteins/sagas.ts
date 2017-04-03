84f70a0eea3c945b0b714e0ae647e6444c64e33d
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-04-03 14:26:56 +0200

    Use stricter tslint exception

--- src/proteins/sagas.ts
+++ src/proteins/sagas.ts
@@ -3,7 +3,7 @@ import { call, put } from 'redux-saga/effects';
 import { fetchFailed, fetchSucceeded } from './actions';
 import { fetchProteins } from './services';
 
-/* tslint:disable:export-name */
+// tslint:disable-next-line:export-name
 export function* fetchProteinsWorker() {
     try {
         const proteins = yield call(fetchProteins);
