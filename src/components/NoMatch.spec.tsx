bdaec122d5daba6a03601ab8955202085a89c14f
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-06-13 14:16:19 +0200

    Fixed no match test

--- src/components/NoMatch.spec.tsx
+++ src/components/NoMatch.spec.tsx
@@ -5,8 +5,11 @@ import { shallow } from 'enzyme';
 import { NoMatch } from './NoMatch';
 
 describe('<NoMatch />', () => {
-    it('should countain link to ligands and proteins viewer', () => {
+    it('should countain link to ligands and proteins viewer and pharmacophores viewer', () => {
         const wrapper = shallow(<NoMatch/>);
-        expect(wrapper.find('Link').prop('to')).toEqual('/ligands-and-proteins');
+        expect(wrapper.find('Link').map((p) => p.prop('to'))).toEqual(expect.arrayContaining([
+            '/ligands-and-proteins',
+            '/pharmacophores'
+        ]));
     });
 });
