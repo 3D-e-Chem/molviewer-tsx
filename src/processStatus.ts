e7953a3e7afb5c63cca74fbd3148b022a12d1c32
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-06-22 13:36:04 +0200

    All 2xx status codes are OK

--- src/processStatus.ts
+++ src/processStatus.ts
@@ -1,12 +1,12 @@
 /**
- * window.fetch does not error on non-200 status codes.
+ * window.fetch does not error on non-2xx status codes.
  * Chain this method as the first promise.then
  *
  * @param response Response
  *
  */
 export const processStatus = (response: Response) => {
-    if (response.status === 200 || response.status === 0) {
+    if ((response.status >= 200 && response.status < 300) || response.status === 0) {
         return Promise.resolve(response);
     } else {
         return Promise.reject(new Error(response.statusText));
