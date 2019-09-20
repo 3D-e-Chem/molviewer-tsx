f2a366c8a2749ffe19dad949d9cf8ae011d600b4
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-09-20 15:50:48 +0200

    Update webpack config to output a library without cache busting filenames

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rsync -a --exclude asset-manifest.json build/static/ ../knime-molviewer/plugin/src/main/js-lib/molviewer
-mkdir ../knime-molviewer/plugin/src/main/js-lib/molviewer/static
-mv ../knime-molviewer/plugin/src/main/js-lib/molviewer/media ../knime-molviewer/plugin/src/main/js-lib/molviewer/static/
+mkdir ../knime-molviewer/plugin/src/main/js-lib/molviewer/css/static
+mv ../knime-molviewer/plugin/src/main/js-lib/molviewer/media ../knime-molviewer/plugin/src/main/js-lib/molviewer/css/static/
