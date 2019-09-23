08cbdc957df13aab8285dd00cb6eb74376658ceb
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-09-23 14:26:13 +0200

    Added ligands and proteins app
    For usage in KNIME Dynamic js node

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
 import { ProteinsViewerApp } from './proteins/app'
-import {LigandsViewerApp } from './ligands/app'
+import { LigandsViewerApp } from './ligands/app'
 import { PharmacophoresViewerApp } from "./pharmacophores/app";
+import { LigandsAndProteinsViewerApp } from "./LigandsAndProteinsViewerApp";
 
-export {ProteinsViewerApp, LigandsViewerApp, PharmacophoresViewerApp}
+export {
+    ProteinsViewerApp, 
+    LigandsViewerApp, 
+    PharmacophoresViewerApp,
+    LigandsAndProteinsViewerApp
+}
